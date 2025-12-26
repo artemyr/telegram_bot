9b3f9f3dd042440c2ec8805dc757f4016d248713
a.riabinin <a.riabinin@notamedia.com.ru>
2025-12-26 11:04:57 +0300

    commands

--- app/Console/Commands/RegisterWebhookCommand.php
+++ app/Console/Commands/RegisterWebhookCommand.php
@@ -6,7 +6,7 @@ use Illuminate\Console\Command;
 
 class RegisterWebhookCommand extends Command
 {
-    protected $signature = 'telegram:webhook:register';
+    protected $signature = 'telegram:hook:set';
     protected $description = 'Зарегистрировать вебхук для бота';
 
     public function handle()
--- app/Console/Commands/RemoveWebhookCommand.php
+++ app/Console/Commands/RemoveWebhookCommand.php
@@ -6,7 +6,7 @@ use Illuminate\Console\Command;
 
 class RemoveWebhookCommand extends Command
 {
-    protected $signature = 'telegram:webhook:remove';
+    protected $signature = 'telegram:hook:remove';
     protected $description = 'Удалить вебхук для бота';
 
     public function handle()

f571fcc755ecad6c6ef5272b323f59dcc6d39f8e
a.riabinin <a.riabinin@notamedia.com.ru>
2026-01-28 17:11:58 +0300

    menu

--- src/Domain/Travel/Menu/MenuFactory.php
+++ src/Domain/Travel/Menu/MenuFactory.php
@@ -12,9 +12,15 @@ class MenuFactory
     public function handle(): MenuItem
     {
         return MenuItem::make(troute('home'), 'Главное меню')
-            ->add(MenuItem::make(troute('find'), '🔍 Найти компанию', fn() => message()->hint('🔍 Найти компанию')))
-            ->add(MenuItem::make(troute('create'), '➕ Создать предложение', fn() => message()->hint('➕ Создать предложение')))
-            ->add(MenuItem::make(troute('profile'), '👤 Мой профиль', fn() => message()->hint('👤 Мой профиль')))
-            ->add(MenuItem::make(troute('how_work'), '❓ Как это работает', fn() => message()->hint('❓ Как это работает')));
+            ->add(MenuItem::make(troute('travel_find'), '🔍 Найти компанию')
+                ->add(MenuItem::make(troute('travel_khutor'), 'Роза хутор'))
+                ->add(MenuItem::make(troute('travel_red'), 'Красная поляна'))
+                ->add(MenuItem::make(troute('travel_gas'), 'Газпром'))
+                ->add(MenuItem::make(troute('travel_sher'), 'Шерегеш'))
+                ->add(MenuItem::make(troute('travel_other'), 'Другое (?)'))
+            )
+            ->add(MenuItem::make(troute('travel_create'), '➕ Создать предложение', fn() => message()->hint('➕ Создать предложение')))
+            ->add(MenuItem::make(troute('travel_profile'), '👤 Мой профиль', fn() => message()->hint('👤 Мой профиль')))
+            ->add(MenuItem::make(troute('travel_how_work'), '❓ Как это работает', fn() => message()->hint('❓ Как это работает')));
     }
 }
